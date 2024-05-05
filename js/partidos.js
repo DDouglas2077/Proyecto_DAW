64b141ac30e4177d0df857141099dcd4f99afa63
castillo1103 <125770998+castillo1103@users.noreply.github.com>
2024-05-04 23:54:18 -0600

    arreglando

---
+++ js/partidos.js
@@ -0,0 +1,44 @@
+import{
+    SavePartidos,
+    deletePartido,
+    updatePartido,
+    onGetPartido,
+    onAuthStateChanged,
+    onGetJugador,
+    onGetTorneo,
+    auth
+} from "./main.js";
+
+function ValidacionPartidos() {
+    const nombreTorneo = document.getElementById("selectTorneo").value;
+    const nombreJugador1 = document.getElementById("selectJugador1").value;
+    const nombreJugador2 = document.getElementById("selectJugador2").value;
+    const fechaEmision = document.getElementById("fechaEmision").value;
+    const horaInicio = document.getElementById("horaInicio").value;
+
+    if (!nombreTorneo){
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups',
+            text: 'Seleccione un torneo',
+        });
+        return false;
+    }
+     if (!nombreJugador1 || !nombreJugador2){
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups',
+            text: 'Seleccione ambos jugadores',
+
+        });
+        return false;
+     }
+     if(nombreJugador1 === nombreJugador2){
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups',
+            text: 'Los jugadores deben ser diferentes',
+        });
+        return false;
+     }
+}
